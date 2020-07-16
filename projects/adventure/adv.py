c434fe9045f417b8dfcd2debef42154ac596e9b3
GeraldRyan <gerald.ryan40@yahoo.com>
2020-07-16 18:13:03 -0500

    made a master plan of rooms visitied

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -11,8 +11,8 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+# map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
@@ -30,42 +30,58 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n', 'n']
 traversal_path = []
+master_plan = {}
 
 
-def bft(starting_vertex, ):
+def bft(starting_vertex):
     """
     Print each vertex in breadth-first order
     beginning from starting_vertex.
     """
     # create empty queue
     counter = 1
-    print("starting vertex", starting_vertex)
     q = []
     # ceate set to store visited nodes
     visited = set()
     traversal_path = []
+    master_plan[starting_vertex.id] = starting_vertex.get_exits()
+    print("Master plan", master_plan)
     # initialize starting note
     q.append(starting_vertex)
     # while queue isn't empty
     while len(q) > 0:
+
         print("counter: ", counter)
         counter += 1
+        q_ids = []
+        for i in q:
+            q_ids.append(i.id)
         # dequeue first item
-        print("q[0] id:", q[0].id)
-        v_obj = q.pop(0)
-        print('v_obj id:', v_obj.id)
+        # print("q[0] id:", q[0].id)
+        v_obj = q.pop()
+        print("queue ids", q_ids)
+
+        if v_obj.id not in master_plan:
+            master_plan[v_obj.id] = v_obj.get_exits()
+            print("master plan", master_plan)        
+
 
         if v_obj.id not in visited:
             visited.add(v_obj.id)
-            print('visited', visited)
+            # print('visited', visited)
             # Do something with node
             # add all neighbors to queue
-            for direction in v_obj.get_exits():  # Need to find the get neighbors function
+        # elif v in visited:
+            # instruct it to not return whence it came somehow
+            for direction in v_obj.get_exits():  # This is doing it too fast. Getting them all at once. Have to go back to queue after having gotten one. Have to call queue. 
                 traversal_path.append(direction)
-                print("direction", direction)
+                print('v_obj id:', v_obj.id)
+                print('v_obj get exits:', v_obj.get_exits())
+                print("Traversal Path", traversal_path)
+                # print("direction", direction)
                 next_room = v_obj.get_room_in_direction(direction)
                 q.append(next_room)
-
+    print("final rooms visited", visited)
     return traversal_path
 
 traversal_path = bft(world.starting_room)
@@ -80,9 +96,9 @@ for move in traversal_path:
     player.travel(move)
     visited_rooms.add(player.current_room)
     print("------------------\ncurrent room", player.current_room, '\n------------------\n--------------------')
-    print("Current room exits:", player.current_room.get_exits())
-    print("Current room exits string:", player.current_room.get_exits_string())
-    print("Current room desc:", player.current_room.description)
+    # print("Current room exits:", player.current_room.get_exits())
+    # print("Current room exits string:", player.current_room.get_exits_string())
+    # print("Current room desc:", player.current_room.description)
     visited_rooms_printer = set()
     for room in visited_rooms:
         visited_rooms_printer.add(room.id)
